a52474e63393c431a92a53633080a0d2abb8c48a
robbieculkin <robbieculkin@gmail.com>
2019-11-14 21:00:34 -0800

    modified depth_map.py

--- depth_map.py
+++ depth_map.py
@@ -4,20 +4,23 @@ from matplotlib import pyplot as plt
 import os
 
 # frame = file name of .png file
-def createMap(frame):
+def createMap(rel_path, frame):
     code_dir = os.path.dirname(__file__)
-    rel_path = "KITTI/data_scene_flow/testing/"
 
     left_path = os.path.join(code_dir, (rel_path+"image_2/"+frame))
     right_path = os.path.join(code_dir, (rel_path+"image_3/"+frame))
-
 
     imgL = cv2.imread(left_path, 0)
     imgR = cv2.imread(right_path, 0)
 
     stereo = cv2.StereoBM_create(numDisparities=64, blockSize=9)
     disparity = stereo.compute(imgL, imgR)
+    return disparity
+    
+
+if __name__ == '__main__':
+    rel_path = "KITTI/data_scene_flow/testing/"
+
+    disparity = createMap(rel_path, "000183_11.png")
     plt.imshow(disparity, 'gray')
     plt.show()
-
-createMap("000183_11.png")
